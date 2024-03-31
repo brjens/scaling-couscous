371cdeebd7fb2f047adecb465ca160406248de3a
Brandon Jensen <brjensen121@gmail.com>
2024-03-30 23:49:21 -0400

    added a function which changes the url, as links are clicked. it takes what ever the href is and sets that as a path

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,21 @@
 const Navbar = () => {
+    
+    const handleButtonClick = (event) => {
+        window.location.href = event.target.href;
+    }
+    
     return ( 
         <nav className="navbar">
             <div className="links">
-                <a href="/">home</a>
-                <a href="/contact">contact</a>
-                <a href="/about">about</a>
-                <a href="/estimate" style={{
+                <a href="/" onClick={handleButtonClick}>home</a>
+                <a href="/contact" onClick={handleButtonClick}>contact</a>
+                <a href="/about" onClick={handleButtonClick}>about</a>
+                <a href="/estimate" onClick={handleButtonClick} style={{
                     color: "white",
                     backgroundColor: "#e0842e",
                     borderRadius: "8px" 
                 }}>estimate</a>
+                <a href="/service-call" onClick={handleButtonClick}>service call</a>
             </div>
         </nav>
      );
