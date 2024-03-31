ab1f83c8194ebbce508f948c37f7264416213f5f
Brandon Jensen <brjensen121@gmail.com>
2024-03-30 23:45:14 -0400

    Added some conditionals that display a central component based off what the user clicks on the NavBar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import Header from './Header';
 import ServiceCall from './ServiceCall';
 import Footer from './Form';
 function App() {
+  let content = <Home />;
 
+  if(window.location.pathname === '/service-call') {
+    content = <ServiceCall />;
+  }
+
+  if(window.location.pathname === '/contact') {
+    // TODO: Add Contact and uncomment below
+    // content = <Contact />;
+  }
+
+  if(window.location.pathname === '/about') {
+    // TODO: Add About and uncomment below
+    // content = <About />;
+  }
+
+  if(window.location.pathname === '/estimate') {
+    // TODO: Add Estimate and uncomment below
+    // content = <Estimate />;
+  }
   return (
     <div className={`App`}>
         <Header />
         <Navbar />
       <div className="content">
-        <ServiceCall />
+      {content}
       </div>
         <Footer />
     </div> 
