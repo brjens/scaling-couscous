5dfd2f82f55d4a30a47a1a44348708da8a0b3aee
Brandon Jensen <brjensen121@gmail.com>
2024-04-07 19:49:38 -0400

    implementated slick Carousel with place-holders

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,36 @@
-const home = () => {
-    return ( 
-        <div className="home">
-            <h2>Home Page</h2>
-        </div>
-     );
+import React from "react";
+import Slider from "react-slick";
+
+export default function Home() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    //variableWidth: true,
+   // variableHeight: true,
+  };
+  return (
+    <Slider {...settings}>
+        <div>
+        <img src="https://placehold.co/600x400" alt="Image 1" />
+      </div>
+      <div>
+        <img src="https://placehold.co/600x400" alt="Image 2" />
+      </div>
+      <div>
+        <img src="https://placehold.co/600x400" alt="Image 3" />
+      </div>
+      <div>
+        <img src="https://placehold.co/600x400" alt="Image 4" />
+      </div>
+      <div>
+        <img src="https://placehold.co/600x400" alt="image 5" />
+      </div>
+      {/* Add more divs with images as needed */}
+    </Slider>
+  );
 }
- 
-export default home;
